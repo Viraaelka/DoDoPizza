014b313f02597cc77de55e248231710e3e3f9dd6
halilovaelvira <aelya089@mail.ru>
2019-10-07 17:46:02 +0300

    check new method

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,8 +3,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.Set;
 
 public class TestRunner extends BeforeAfterSetup{
     MainTestClass mainTestClass = PageFactory.initElements(driver, MainTestClass.class);
@@ -25,5 +28,35 @@ public class TestRunner extends BeforeAfterSetup{
         System.out.println(count + " getTitle() = " + webDriver.getTitle());
         //   return navigationMenu.stream().filter(webElement -> webElement.isDisplayed()).peek();
        // return count;
+    }
+
+    @Test
+    public void someSimpleTest() {
+        driver.get("http://internetka.in.ua");
+
+        String originalWindow = driver.getWindowHandle();
+        final Set<String> oldWindowsSet = driver.getWindowHandles();
+
+        driver.findElement(By.tagName("a")).click();
+
+        String newWindow = (new WebDriverWait(driver, 10))
+                .until(new ExpectedCondition<String>() {
+                           public String apply(WebDriver driver) {
+                               Set<String> newWindowsSet = driver.getWindowHandles();
+                               newWindowsSet.removeAll(oldWindowsSet);
+                               return newWindowsSet.size() > 0 ?
+                                       newWindowsSet.iterator().next() : null;
+                           }
+                       }
+                );
+
+        driver.switchTo().window(newWindow);
+
+        System.out.println("New window title: " + driver.getTitle());
+        driver.close();
+
+        driver.switchTo().window(originalWindow);
+        System.out.println("Old window title: " + driver.getTitle());
+
     }
 }
