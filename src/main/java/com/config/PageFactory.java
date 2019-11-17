6862f7341fa7fab3c719ed7e538320baf41df5e3
Viraaelka <01101995Aa>
2019-11-17 20:57:01 +0300

    a few changes were done

--- src/main/java/com/config/PageFactory.java
+++ src/main/java/com/config/PageFactory.java
@@ -8,16 +8,12 @@ import java.util.concurrent.TimeUnit;
 
 public class PageFactory {
     private static WebDriver driver;
-
-    //private static String driverPath = "/Users/halilovaelvira/IdeaProjects/MyOwnAE2/chromedriver";
     private static String driverPath = "/home/elvira/IdeaProject/Utils/chromedriver";
     public static String website = "https://dodopizza.com/germantown";
     //  public static String website = "https://dodopizza.ru/petergof/";
 
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
-        //   options.addArguments("--user-data-dir=/Users/halilovaelvira/Library/Application Support/Google/Chrome/");
-        //   options.addArguments("--profile-directory=NewProfile_1");
         System.setProperty("webdriver.chrome.driver", getDriverPath());
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
