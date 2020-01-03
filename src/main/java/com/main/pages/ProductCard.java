f27134331640a16821890652901ba283a89cfc82
Viraaelka <aelya089@mail.ru>
2020-01-03 19:54:23 +0300

    Uploading the method to test the product

--- src/main/java/com/main/pages/ProductCard.java
+++ src/main/java/com/main/pages/ProductCard.java
@@ -1,16 +1,15 @@
 package com.main.pages;
 
+import com.config.PageFactory;
 import io.cucumber.datatable.DataTable;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 import java.util.Map;
 
 public class ProductCard extends WrapperPage {
-    @FindBy(xpath = "//div[contains(@class, 'ProductCard')]")
-    WebElement sizeAndPastry;
 
     public ProductCard(WebDriver driver) {
         super(driver);
@@ -18,18 +17,19 @@ public class ProductCard extends WrapperPage {
 
     public void checkSizeAndPastry(DataTable dataTable) {
         Map<String, String> dataMap = dataTable.asMap(String.class, String.class);
+        String xpathEnd = "//div[contains(@class, 'ProductCard')]//span[contains(text(), '%s')]";
         dataMap.forEach((k, v) -> {
-            StringBuilder temp = new StringBuilder();
+            WebElement element = null;
             switch (k) {
                 case "Size":
-                    temp.append(v).append("\", ");
+                    element = PageFactory.getDriver().findElement(By.xpath(String.format(xpathEnd, v)));
+                    Assert.assertEquals("Incorrect value of size and pastry", element.getText().replaceAll("\\D", ""), v);
                     break;
                 case "Kind of pastry":
-                    temp.append(v);
+                    element = PageFactory.getDriver().findElement(By.xpath(String.format(xpathEnd, v)));
+                    Assert.assertEquals("Incorrect value of size and pastry", element.getText().replaceAll("\\d+\\W+", ""), v.toLowerCase());
                     break;
             }
-            System.out.println("Siiiize = " + temp);
-            Assert.assertEquals("Incorrect value of size and pastry", sizeAndPastry.getText(), temp);
         });
     }
 }
