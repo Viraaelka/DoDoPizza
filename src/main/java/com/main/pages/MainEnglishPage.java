76a7b96e1155a82ef605f5bd5f7cbbe832085439
Viraaelka <aelya089@mail.ru>
2020-01-10 20:45:42 +0300

    work in progress -> just prevent my code from accident removal

--- src/main/java/com/main/pages/MainEnglishPage.java
+++ src/main/java/com/main/pages/MainEnglishPage.java
@@ -1,21 +1,27 @@
 package com.main.pages;
 
+import com.config.PageFactory;
 import com.main.exceptions.AutotestException;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class MainEnglishPage extends WrapperPage {
-    public static String itemXpath = "//div[@class='page__wrapper']//*[contains(text(),'%s')]";
+    public static String itemXpath = "//div[@class='page__wrapper']//*[contains(text(),'%s')]//ancestor::div[contains(@data-testid,'menu__product')]//button";
     public MainEnglishPage(WebDriver driver) {
         super(driver);
     }
 
     //todo -> stopped the progress here -> continue from this method
     public void selectItem(String itemName){
+        WebElement addToCartButton;
         try{
-
+            addToCartButton = PageFactory.getDriver().findElement(By.xpath(String.format(itemXpath, itemName)));
+            addToCartButton.click();
+            Assert.assertTrue(String.format("Unable to find %s element", itemName), addToCartButton.getText(), );
         }
         catch(NoSuchElementException e){
             throw new AutotestException(String.format("Unable to find the item \"%s\"",itemName));
